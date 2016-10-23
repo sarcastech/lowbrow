6839ad1300b3a1a66bb3657c1e3cc5c9e58b22f8
s tech <email@sarcas.tech>
2016-10-23 18:12:22 -0400

    move error handling to a callback function

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 let fs = require('fs')
 let exec = require('child_process').exec
+let noop = function () {}
 
-function lowbrow (fileName, site) {
+function lowbrow (fileName, site, callback) {
   let regex = new RegExp(`^${site}`)
   fs.readFile(fileName, 'utf8', function (err, data) {
     if (err) {
-      console.log('error: ', err)
-      return false
+      return callback(err)
     }
 
     let dataArray = data.split('\n')
@@ -16,14 +16,7 @@ function lowbrow (fileName, site) {
       return domain.match(regex)
     })
 
-    exec(`open http://${ip.split(': ')[1]}`, function (err, stdout) {
-      if (err) {
-        console.log('Exec Error: ', err)
-        return false
-      }
-
-      console.log(`opened ${site}`)
-    })
+    exec(`open http://${ip.split(': ')[1]}`, callback || noop)
   })
 }
 
