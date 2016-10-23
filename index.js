36f7da6d1d3eee6a2b28b7208ada41b2f17efa9b
s tech <email@sarcas.tech>
2016-10-23 17:49:22 -0400

    parameterize target file to read

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 let fs = require('fs')
 let exec = require('child_process').exec
 
-function lowbrow (site) {
+function lowbrow (fileName, site) {
   let regex = new RegExp(`^${site}`)
-  fs.readFile('blah.txt', 'utf8', function (err, data) {
+  fs.readFile(fileName, 'utf8', function (err, data) {
     if (err) {
       console.log('error: ', err)
       return false
