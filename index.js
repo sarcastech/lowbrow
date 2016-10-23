19ffd6b89c2e96236e196057546548cc2e3c1428
s tech <email@sarcas.tech>
2016-10-23 14:38:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let fs = require('fs')
+let exec = require('child_process').exec
+
+function lowbrow (site) {
+  let regex = new RegExp(`^${site}`)
+  fs.readFile('blah.txt', 'utf8', function (err, data) {
+    if (err) {
+      console.log('error: ', err)
+      return false
+    }
+
+    let dataArray = data.split('\n')
+    let ip = dataArray.find(function (domain) {
+      return domain.match(regex)
+    })
+
+    exec(`open http://${ip.split(': ')[1]}`)
+  })
+}
+
+module.exports = lowbrow
