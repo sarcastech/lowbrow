fc50c0100391bd123390b2221414b381c9908677
s tech <email@sarcas.tech>
2016-12-06 22:48:12 -0500

    replace child process ".exec()" with ".spawn()"

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const WRITABLE = require('stream').Writable
-const EXEC = require('child_process').exec
+const SPAWN = require('child_process').spawn
 let writeStream = new WRITABLE()
 
-writeStream._write = function (chunk, encoding, callback) {
-  EXEC(`open http://${chunk.toString()}`, callback)
+writeStream._write = function (chunk, encoding, done) {
+  SPAWN('open', [`http://${chunk.toString()}`])
+  writeStream.end()
+  done()
 }
 
 module.exports = writeStream
