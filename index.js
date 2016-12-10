0d5af7f8cfcd25ffd503b7910a09b9ef5b627495
s tech <email@sarcas.tech>
2016-12-09 21:20:06 -0500

    move transform instantiation to within function

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 const WRITABLE = require('stream').Writable
 const SPAWN = require('child_process').spawn
-let writeStream = new WRITABLE()
 
-writeStream._write = function (chunk, encoding, done) {
-  SPAWN('open', [`http://${chunk.toString()}`])
-  writeStream.end()
-  done()
+module.exports = function () {
+  const writeStream = new WRITABLE()
+  writeStream._write = function (chunk, encoding, done) {
+    SPAWN('open', [`http://${chunk.toString()}`])
+    writeStream.end()
+    done()
+  }
 }
-
-module.exports = writeStream
